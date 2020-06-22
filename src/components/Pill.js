c9499fd5f93073368220c09fbc6ad977a9ddd022
Jason Welsh <jasonbwelsh@gmail.com>
2020-06-22 19:03:18 -0400

    static UI complete

--- src/components/Pill.js
+++ src/components/Pill.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { StyledPill } from './StyledPill.js';
+import { ReactComponent as MinusIcon } from '../icons/remove-outline.svg';
+import { ReactComponent as PlusIcon } from '../icons/add-outline.svg';
 
 function Pill({ count, handleCountChange }) {
   return (
     <StyledPill>
-      <button onClick={() => handleCountChange(-1)}>-</button>
-      <span>{count}</span>
-      <button onClick={() => handleCountChange(1)}>+</button>
+      <div
+        className="count-btn count-btn__decrement"
+        onClick={() => handleCountChange(-1)}
+      >
+        <MinusIcon className="icon" height="32px" width="32px" />
+      </div>
+      <span className="count-display">{count}</span>
+      <div
+        className="count-btn count-btn__increment"
+        onClick={() => handleCountChange(1)}
+      >
+        <PlusIcon className="icon" height="32px" width="32px" />
+      </div>
     </StyledPill>
   );
 }
--- src/components/StyledPill.js
+++ src/components/StyledPill.js
@@ -1,3 +1,63 @@
 import styled from 'styled-components';
 
-export const StyledPill = styled.div``;
+export const StyledPill = styled.div`
+  padding: 0 6px;
+  height: 40px;
+  width: 130px;
+  border-radius: 20px;
+  background: #fff;
+  box-shadow: 4px 8px 12px 0 rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .count-btn {
+    padding: 0;
+    cursor: pointer;
+    background: transparent;
+    border-radius: 50%;
+    height: 32px;
+    width: 32px;
+  }
+
+  .count-btn__decrement {
+    transition: all 0.3s ease;
+    &:hover {
+      background: #e26e6e;
+    }
+
+    &:hover line {
+      stroke: #fff !important;
+    }
+
+    line {
+      stroke: #e26e6e !important;
+      transition: all 0.3s ease;
+    }
+  }
+
+  .count-btn__increment {
+    transition: all 0.3s ease;
+
+    &:hover {
+      background: #8ed273;
+    }
+
+    &:hover line {
+      stroke: #fff !important;
+    }
+
+    line {
+      stroke: #8ed273 !important;
+      transition: all 0.3s ease;
+    }
+  }
+
+  .count-display {
+    font-weight: 600;
+    font-size: 20px;
+    color: #6a85a0;
+    line-height: 40px;
+    text-align: center;
+  }
+`;
